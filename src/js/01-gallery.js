d85fb185e104d008ed40cec0de301ed644c5cf2e
Stefan Dunikovskyi <stefan090304@gmail.com>
2022-06-08 18:15:54 +0200

    add first

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,39 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Change code below this line
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const galleryWrapper = document.querySelector('.gallery');
+
+const createMarkup = () => {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </div>`
+    )
+    .join('');
+};
+function onClickImg(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  var lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+  console.log(lightbox);
+}
+
+galleryWrapper.innerHTML = createMarkup();
+galleryWrapper.addEventListener('click', onClickImg);
